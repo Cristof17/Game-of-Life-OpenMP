75b147ccc9dfbbb3425fcff2d4add0b1d9f05c5e
Cristof17 <cristof_rotsching@yahoo.com>
2016-11-05 18:05:21 +0200

    Solved initialization issue + added tests

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ serial: g_serial.cpp
 	g++ g_serial.cpp -o g_serial
 parallel: g_parallel.cpp
 	g++ g_parallel.cpp -o g_omp
+run_serial:
+	./g_serial input/test1.txt 100 out/out1.txt
--- g_serial.cpp
+++ g_serial.cpp
@@ -18,33 +18,37 @@ char **matrix;
 bool **bools;
 bool **bools2;
 
-void extend_with_margins(int L, int C, bool **begin, bool **end){
+void extend_with_margins(int L, int C, bool **begin, bool ***end){
 
-	end =(bool **)malloc((L + 2) * sizeof(bool *));
+	(*end) =(bool **)malloc((L + 2) * sizeof(bool *));
 	for (int i = 0; i < L; ++i){
-		end[i] = (bool *)malloc((C + 2) * sizeof(bool));
+		(*end)[i] = (bool *)malloc((C + 2) * sizeof(bool));
+	}
+	for (int i = 0; i < L; ++i){
+		(*end)[i + 1][0] = begin[i][C - 1];
+		(*end)[i + 1][C - 1 + 1] = begin[i][0];
 	}
 
-	for (int i = 0; i < L; ++i){
-		end[i + 1][0] = begin[i][C - 1];
-		end[i + 1][C - 1 + 1] = begin[i][0];
-	}
+	cout << "C =" << C << "L =" << L << endl;
+	
+	/*
 
 	for (int i = 0; i < C; ++i){
-		end[0][i + 1] = begin[0][i];
-		end[L - 1 + 1][0] = begin[0][i];
+		(*end)[0][i + 1] = begin[0][i];
+		(*end)[L - 1 + 1][0] = begin[0][i];
 	}
 
-	end[0][0] = begin[L-1][C-1];
-	end[L- 1 + 1][0] = begin[0][C-1];
-	end[L - 1 + 1][C -1 + 1] = begin[0][0];
-	end[0][C - 1 + 1] = begin[L-1][C-1];
+	(*end)[0][0] = begin[L-1][C-1];
+	(*end)[L- 1 + 1][0] = begin[0][C-1];
+	(*end)[L - 1 + 1][C -1 + 1] = begin[0][0];
+	(*end)[0][C - 1 + 1] = begin[L-1][C-1];
 
 	for (int i = 0; i < L; ++i){
 		for (int j = 0; j < C; ++j){
-			end[i+1][j+1] = begin[i][j];
+			(*end)[i+1][j+1] = begin[i][j];
 		}
 	}
+	*/
 
 }
 
@@ -72,6 +76,15 @@ void simulate_matrix(int L,int C,bool **start,bool ** end){
 			
 }
 
+void print(bool **mat, int L, int C){
+	for (int i = 0; i < L; ++i){
+		for (int j = 0; j < C; ++j){
+			cout << mat[i][j] << " ";
+		}
+	cout << endl;
+	}
+}
+
 int main(int argc, char **argv){
 	
 	if (argc <= 1){
@@ -79,17 +92,7 @@ int main(int argc, char **argv){
 		return -1;
 	}
 
-	matrix =(char **)malloc(L * sizeof(char *));
-	for (int i = 0; i < L; ++i){
-		matrix[i] = (char *)malloc(C * sizeof(char));
-	}
 
-	bools = (bool **)malloc(L * sizeof(bool*));
-	bools2 = (bool **)malloc(L * sizeof(bool*));
-	for (int i = 0; i < L; ++i){
-		bools[i] = (bool*)malloc(C * sizeof(bool));
-		bools2[i] = (bool*)malloc(C * sizeof(bool));
-	}
 
 	fisier_in = argv[1];
 	N = atoi(argv[2]);
@@ -97,8 +100,18 @@ int main(int argc, char **argv){
 
 	FILE *f_in = fopen(fisier_in, "rw+");
 	FILE *f_out = fopen(fisier_out, "rw+");
-
 	fscanf(f_in, "%d %d\n", &L, &C);	
+
+	matrix =(char **)malloc(L * sizeof(char *));
+	for (int i = 0; i < L; ++i){
+		matrix[i] = (char *)malloc(C * sizeof(char));
+	}
+
+	bools = (bool **)malloc(L * sizeof(bool*));
+	for (int i = 0; i < L; ++i){
+		bools[i] = (bool*)malloc(C * sizeof(bool));
+	}
+
 	for (int i = 0; i < L; ++i){
 		for (int j = 0; j < C; ++j){
 			fscanf(f_in, "%c ", &matrix[i][j]);
@@ -106,11 +119,15 @@ int main(int argc, char **argv){
 	}
 
 	transform_in_bool(L, C, matrix, bools);
-	extend_with_margins(L, C, bools, bools2); 
+	print(bools, L, C);
+
+	extend_with_margins(L, C, bools, &bools2); 
+	/*
+	print(bools2, L + 2, C + 2);
 	for (int i = 0; i < N/2; ++i){
 		simulate_matrix(L, C, bools2, bools);
 		simulate_matrix(L, C, bools, bools2);
-	}
+	}*/
 	
 	return 0;
 }
