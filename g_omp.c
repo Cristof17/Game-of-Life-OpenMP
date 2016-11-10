3f1c4a25a4410cfd89cd6ea30f16ad3d284c1146
Cristof17 <cristof_rotsching@yahoo.com>
2016-11-10 17:00:58 +0200

    Rewrote README

--- g_omp.c
+++ g_omp.c
@@ -19,31 +19,29 @@ char **matrix;
 int **bools;
 int **bools_normal;
 int **bools_buffered;
+int **current;
+int **current_buffered;
 
-void extend_with_margins(int L, int C, int **begin, int ***end){
+void extend_with_margins(int L, int C, int **begin, int **end){
 
-	(*end) =(int **)malloc((L + 2) * sizeof(int *));
-	for (int i = 0; i < L + 2; ++i){
-		(*end)[i] = (int *)malloc((C + 2) * sizeof(int));
-	}
 	for (int i = 0; i < L; ++i){
-		(*end)[i + 1][0] = begin[i][C - 1];
-		(*end)[i + 1][C - 1 + 1] = begin[i][0];
+		end[i + 1][0] = begin[i][C - 1];
+		end[i + 1][C + 1] = begin[i][0];
 	}
 
 	for (int i = 0; i < C; ++i){
-		(*end)[0][i + 1] = begin[0][i];
-		(*end)[L - 1 + 1][0] = begin[0][i];
+		end[0][i + 1] = begin[L - 1][i];
+		end[L + 1][i + 1] = begin[0][i];
 	}
 
-	(*end)[0][0] = begin[L-1][C-1];
-	(*end)[L- 1 + 1][0] = begin[0][C-1];
-	(*end)[L - 1 + 1][C -1 + 1] = begin[0][0];
-	(*end)[0][C - 1 + 1] = begin[L-1][C-1];
+	end[0][0] = begin[L-1][C-1];
+	end[L + 1][0] = begin[0][C-1];
+	end[L + 1][C + 1] = begin[0][0];
+	end[0][C + 1] = begin[L-1][C-1];
 
 	for (int i = 0; i < L; ++i){
 		for (int j = 0; j < C; ++j){
-			(*end)[i+1][j+1] = begin[i][j];
+			end[i+1][j+1] = begin[i][j];
 		}
 	}
 
@@ -65,7 +63,6 @@ void simulate_matrix(int L,int C,int **start,int **end){
 	int alive = 0;
 	int dead = 0;
 
-	#pragma omp parallel for reduction(+: alive, dead)
 	for (int i = 1; i < L - 1 ; ++i){ //start from (1,1) because we have added borders
 		for (int j = 1; j < C - 1; ++j){ //start from (1,1) because we have added borders
 			//count alive and dead neighbors
@@ -81,14 +78,12 @@ void simulate_matrix(int L,int C,int **start,int **end){
 			//check the values and put them in the buffered matrix
 			if (alive < 2)
 				end[i][j] = DEAD;
-			else if (start[i][j] == ALIVE && (alive == 2 || alive == 3))
+			if (start[i][j] == DEAD && alive == 3)
 				end[i][j] = ALIVE;
-			else if (alive > 3)
+			if (alive > 3)
 				end[i][j] = DEAD;
-			else if (start[i][j] == DEAD && alive == 3)
+			if (start[i][j] == ALIVE && (alive == 2 || alive == 3))
 				end[i][j] = ALIVE;
-			else
-				end[i][j] = start[i][j];
 			alive = 0;
 			dead = 0;
 		}
@@ -112,8 +107,8 @@ void alloc_buffer_matrix(int L, int C, int ***bools){
 }
 
 void save_to_file(FILE *file, int L, int C, int **matrix){
-	for (int i = 1; i < L-1; ++i){
-		for (int j = 1; j < C -1; ++j){
+	for (int i = 0; i < L; ++i){
+		for (int j = 0; j < C; ++j){
 			if (matrix[i][j] == DEAD)
 				fprintf(file, ". ");
 			else
@@ -125,13 +120,28 @@ void save_to_file(FILE *file, int L, int C, int **matrix){
 }
 
 void copy_matrix(int L, int C, int **start, int **stop){
-	#pragma omp parallel for
 	for (int i = 0; i < L; ++i){
 		for (int j = 0; j < C; ++j){
 			stop[i][j] = start[i][j];
 		}
 	}
 }
+
+void reset_matrix(int L, int C, int **matrix){
+	for (int i = 0; i < L; ++i){
+		for (int j = 0; j < C; ++j){
+			matrix[i][j] = DEAD;
+		}
+	}
+}
+
+void shrink_without_margins(int L, int C, int** start, int **end){
+	for (int i = 1; i < L -1; ++i)
+		for (int j = 1; j < C - 1; ++j)
+			end[i-1][j-1] = start[i][j];
+}
+
+
 
 int main(int argc, char **argv){
 	
@@ -142,8 +152,6 @@ int main(int argc, char **argv){
 		return -1;
 	}
 
-
-
 	fisier_in = argv[1];
 	N = atoi(argv[2]);
 	fisier_out = argv[3];
@@ -152,33 +160,49 @@ int main(int argc, char **argv){
 	FILE *f_out = fopen(fisier_out, "rw+");
 	fscanf(f_in, "%d %d\n", &L, &C);	
 
-	matrix =(char **)malloc(L * sizeof(char *));
-	for (int i = 0; i < L; ++i){
-		matrix[i] = (char *)malloc(C * sizeof(char));
-	}
-
 	bools = (int **)malloc(L * sizeof(int *));
 	for (int i = 0; i < L; ++i){
 		bools[i] = (int *)malloc(C * sizeof(int));
 	}
 
+	bools_normal = (int **) malloc ((L + 2) * sizeof(int *));
+	for (int i = 0; i < L + 2; ++i){
+		bools_normal[i] = (int *)malloc((C + 2)* sizeof(int));
+	}
+
+	//read from input file
+	char c;
 	for (int i = 0; i < L; ++i){
 		for (int j = 0; j < C; ++j){
-			fscanf(f_in, "%c ", &matrix[i][j]);
+			fscanf(f_in, "%c ", &c);
+			switch (c){
+				case '.':{
+					bools[i][j] = DEAD;
+					break;
+				}
+				case 'X':{
+					bools[i][j] = ALIVE;
+					break;
+				}
+					
+			}
 		}
 	}
 
-	transform_in_bool(L, C, matrix, bools);
-	extend_with_margins(L, C, bools, &bools_normal); 
+	//transform_in_bool(L, C, matrix, bools);
 	//from here we will only work with bools_normal and bools_buffered
 	alloc_buffer_matrix(L + 2, C + 2, &bools_buffered);
+	reset_matrix(L + 2, C + 2, bools_normal);
+	reset_matrix(L + 2, C + 2, bools_buffered);
+
+	for (int i = 0; i < N ; ++i){
 	
-	for (int i = 0; i < N; ++i){
+		extend_with_margins(L, C, bools, bools_normal); 
 		simulate_matrix(L + 2, C + 2, bools_normal, bools_buffered);
-		copy_matrix(L + 2, C + 2, bools_buffered, bools_normal);
+		shrink_without_margins(L + 2, C + 2, bools_buffered, bools);
 	}
 
-	save_to_file(f_out, L + 2, C + 2, bools_normal);
+	save_to_file(f_out, L, C, bools);
 	fclose(f_in);
 	fclose(f_out);
 	
